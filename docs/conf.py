932c4bc4d3d88373125ce40fc17038a9e0727f36
Abdullah Makkeh <abdullah.makkeh@gmail.com>
2019-01-03 19:19:17 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-pip install ecos
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
